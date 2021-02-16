93fe0e839227a0c1956ae51b439445a6d6a45834
SaarBS <saar1.bs1@gmail.com>
2021-02-16 16:34:38 +0200

    added toddos

--- Systems/CollectionSystem.java
+++ Systems/CollectionSystem.java
@@ -8,6 +8,7 @@ public class CollectionSystem {
     public final LinearOpMode opMode;
     public final DcMotor motor;
 
+    // TODO: 09/02/2021 initialize motor speed and maybe consider adding also an on off ability as well as toggle 
     public CollectionSystem(LinearOpMode opMode) {
         this.opMode = opMode;
         this.motor = opMode.hardwareMap.get(DcMotor.class, "CollectionMotor");
